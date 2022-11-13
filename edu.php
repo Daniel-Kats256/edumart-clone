43558225bfc6aaf16e0b774d9b8e76e34c24808c
Daniel Devs <110326556+Daniel-Kats256@users.noreply.github.com>
2022-11-13 17:29:03 +0300

    Add files via upload

---
+++ edu.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Document</title>
+		<link rel="stylesheet" href="edu.css" />
+		<style>
+			<?php.require."edu.css"?>
+		</style>
+	</head>
+	<body>
+		<section class="one">
+			<div class="top">
+				<div>
+					<div class="icon">
+						<span><?php include"vectors/instagram.svg"?></span>
+					</div>
+				</div>
+				<div>
+					<div class="icon">
+						<span><?php include"vectors/facebook.svg"?></span>
+					</div>
+				</div>
+				<div>
+					<div class="icon">
+						<span><?php include"vectors/whatsapp.svg"?></span>
+					</div>
+				</div>
+				<div>
+					<div class="icon">
+						<span><?php include"vectors/twitter.svg"?></span>
+					</div>
+				</div>
+				
+				<div>
+					<p>REGISTER</p>
+					<p> APPLY </p>
+					<p> ONLINE</p>
+					<p>BLOG</p>
+					<p> FAQS</p>
+				</div>
+				<select>
+					<option value="USD">USD</option>
+					<option value="ENGLISH">ENGLISH</option>
+				</select>
+				<select>
+					<option value="USD">ENGLISH</option>
+					<option value="ENGLISH">ENGLISH</option>
+				</select>
+			</div>
+			<div class="white">
+				<div style="background-color: blue"></div>
+				<div class="wh1">
+					<h1>Edumart</h1>
+					<div>
+						<div></div>
+						<p>online</p>
+						<div></div>
+					</div>
+				</div>
+				<div class="W1">
+					<div style="border-right: 1px solid grey">
+						<p><span>EMAIL </span><br />info@edumart.org</p>
+					</div>
+					<div style="border-right: 1px solid grey">
+						<p><span>TOLL FREE</span> <br />18 000 000 000</p>
+					</div>
+					<div> student login</div>
+				</div>
+			</div>
+			<div class="green">
+				<nav>
+					<a href="#">HOME</a>
+					<a href="#">ABOUT</a>
+					<a href="#">OUR COURSES</a>
+					<a href="#">GALLEY</a>
+					<a href="#">PAGES</a>
+					<a href="#">CONTACT</a>
+				</nav>
+					<div>
+						<a href="#">SEARCH NOW</a>
+						<div class="now"></div>
+					</div>
+				
+			</div>
+		</section>
+		<div class="main">
+			<div class="box">
+				
+				<div>
+					<h1>APPLY ONLINE</h1>
+					<P>Lorem ipsum, dolor sit   amet consectetur  <br> adipisicing elit. Temporibus, sint.</P>
+					<div></div>
+				</div>
+				<div>
+			
+					<h1>PROSPECTS</h1>
+					<p>Lorem ipsum dolor sit  amet consectetur  <br> adipisicing elit. Quas, harum.</p>
+					<div></div>
+				</div>
+				<div> 
+					<h1>CERTIFICATION</h1>
+					<p>Lorem ipsum dolor sit  amet consectetur  <br> adipisicing elit. Ratione, aliquid.</p>
+					<div></div>
+				</div>
+				
+			</div>
+			<div class="our">
+				
+				<div class="our2">
+					<h3>EXPLORE THE WORLD OF</h3>
+					<h1>OUR GRADUATES</h1>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, velit saepe eveniet <br> dignissimos laboriosam magnam aliquam similique assumenda corrupti vitae!</p>
+					<div>know more </div>
+				</div>
+				<div class="our1"></div>
+			</div>
+			<div class="bc">
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+		</div>
+	</body>
+</html>
